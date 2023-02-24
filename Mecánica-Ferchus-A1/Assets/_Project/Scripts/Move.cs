5537da98232d5dd5b4b3f9db4f4f731123921c8a
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2023-02-24 13:15:22 -0600

    First excersise of 02/24/2023

--- Mecánica-Ferchus-A1/Assets/_Project/Scripts/Move.cs
+++ Mecánica-Ferchus-A1/Assets/_Project/Scripts/Move.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
@@ -6,8 +7,41 @@ public class Move : MonoBehaviour
     [SerializeField] private PhysicsProperties_SO _physicsPropertiesSO;
     [SerializeField] private bool _canMove;
 
+    [SerializeField] private List<Vector3> _forceVectorList;
+
     private void FixedUpdate()
     {
        transform.Translate(_physicsPropertiesSO.Velocity * (_canMove ? 1 : 0) * Time.fixedDeltaTime); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 finalForce = AddForces(_forceVectorList);
+
+            if (finalForce != Vector3.zero)
+            {
+                Debug.LogError("No se puede seguir con esta traslación, pues tenemos una fuerza de: " + finalForce);
+                _canMove = false;
+            }
+
+            else
+            {
+                Debug.Log("Todo bien");
+            }
+        }
+    }
+
+    private Vector3 AddForces(List<Vector3> forceVectorList)
+    {
+        Vector3 result = Vector3.zero;
+
+        for (int i = 0; i < forceVectorList.Count; i++)
+        {
+            result += forceVectorList[i];
+        }
+
+        return result;
     }
 }
