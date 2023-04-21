2b98e59e06080eeb28f8dead2505162041d56fbd
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2023-04-20 19:29:57 -0600

    FluidDrag working

---
+++ Mecánica-Ferchus-A1/Assets/_Project/Scripts/FluidDrag.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class FluidDrag : MonoBehaviour
+    {
+        [Range(1, 2), SerializeField] private float dragExponent = 1;
+        [SerializeField] private float dragCoefficient;
+        private PhysicsEngine _physicsEngine;
+        
+        private void Awake()
+        {
+            _physicsEngine = GetComponent<PhysicsEngine>();
+        }
+
+        private void FixedUpdate()
+        {
+            float speed = _physicsEngine.Velocity.magnitude;
+            float dragMagnitude = dragCoefficient * Mathf.Pow(speed, dragExponent);
+            Vector3 dragForce = dragMagnitude * -_physicsEngine.Velocity.normalized;
+            _physicsEngine.AddForce(dragForce);
+        }
+    }
+}
+
+
